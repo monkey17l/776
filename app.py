df43337e635215bb75058b83c64ccbb6b5f90741
monkey17l <2553s2610532@g.cnees.kr>
2026-05-29 13:22:59 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,19 @@
 import streamlit as st
-st.title ('고양이는 귀엽다')
-st.write('원숭이도 귀엽다')
+import random
+
+# 웃긴 말 리스트
+jokes = [
+    "배고픈데 다이어트 중이면 그건 정신력 RPG",
+    "월요일은 버그처럼 갑자기 찾아온다",
+    "커피를 마셨더니 사람이 된 기분이다",
+    "5분만 쉰다 = 2시간 순삭",
+    "내 통장도 다이어트 중",
+    "코딩은 원인을 찾으면 새로운 오류가 나온다"
+]
+
+st.title("🤣 웃긴 말 생성기")
+
+st.write("버튼을 누르면 웃긴 말을 해줍니다!")
+
+if st.button("웃긴 말 보기"):
+    st.success(random.choice(jokes))
